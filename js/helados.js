55a53951a31f543e86d26aa694a58759a4983cbe
Miguel Suarez <127339719+MiguelSuarez11@users.noreply.github.com>
2024-09-27 12:18:26 -0500

    Initial commit

---
+++ js/helados.js
@@ -0,0 +1,33 @@
+/* Esta lista podría ser reemplazada por la respuesta de un backend */
+const bicicletas = [
+    {
+      id:1,
+      nombre: "Copa Banana y 2 Malteadas de 16o",
+      precio: 13000,
+    },
+    {
+      id:2,
+      nombre: "Sundae Salsa Chocolate Caliente",
+      precio: 4000,
+    },
+    {
+      id:3,
+      nombre: "Salsa Caramelo",
+      precio: 7000,
+    },
+    {
+      id:4,
+      nombre: "Fruta Cereza",
+      precio: 7700,
+    },
+    {
+      id:5,
+      nombre: "Sundae Salsa Chicle",
+      precio: 5000,
+    },
+    {
+      id:6,
+      nombre: "Mixer Galleta",
+      precio: 6600,
+    }
+  ]
